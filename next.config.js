5c4c38dc7aa6d1da983118322dacb5ff8bf1440b
Gezim Hoxha <hgezim@gmail.com>
2023-07-31 05:06:13 +0000

    Try to fix nextra routing

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withNextra = require('nextra')({
 module.exports = withNextra(
   {
     i18n: {
-      locales: ['en', 'zh', 'de'],
+      locales: ['en', 'zh'],
       defaultLocale: 'en'
     }
   }
