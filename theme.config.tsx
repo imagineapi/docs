6d48f38b5737857c0ee9026ea6a5a6d3e2cdd361
Gezim Hoxha <hgezim@gmail.com>
2023-08-19 05:49:15 +0000

    Add posthog script

--- theme.config.tsx
+++ theme.config.tsx
@@ -18,6 +18,9 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: 'https://github.com/imagineapi/docs',
   head: (
     <>
+      <script type="text/javascript" dangerouslySetInnerHTML={{__html: `
+      !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+      posthog.init('phc_WqeHVVgRUV2bUHZSIQEBM3QRprwIfO2BWwHgdU4wo2L',{api_host:'https://ph.imagineapi.dev'})`}}></script>
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://docs.imagineapi.dev/social.png" />
       <link rel="icon" type="image/png" href="/favicon.png" />
