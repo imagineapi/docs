a3326d255f80a36143208267f79f70e03e7a06dc
Gezim Hoxha <hgezim@gmail.com>
2023-09-18 21:56:12 +0000

    Move upgrade and troubleshooting to self-host folder

--- next.config.js
+++ next.config.js
@@ -1,28 +1,36 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra(
-  {
-    i18n: {
-      locales: ['en', 'zh'],
-      defaultLocale: 'en'
+module.exports = withNextra({
+  i18n: {
+    locales: ["en", "zh"],
+    defaultLocale: "en",
+  },
+  redirects: () => [
+    {
+      source: "/api/:slug*",
+      destination: "/aypeeeye/:slug*", // Matched parameters can be used in the destination
+      permanent: true,
     },
-    async redirects() {
-      return [
-        {
-          source: '/api/:slug*',
-          destination: '/aypeeeye/:slug*', // Matched parameters can be used in the destination
-          permanent: true,
-        },
-        // redirect /install to /self-host/install
-        {
-          source: '/install/:slug*',
-          destination: '/self-host-install/:slug*',
-          permanent: true,
-        }
-      ]
+    // redirect /install to /self-host/install
+    {
+      source: "/install/:slug*",
+      destination: "/self-host-install/:slug*",
+      permanent: true,
     },
-  }  
-)
+    // redirect /troubleshooting to /self-host/troubleshooting
+    {
+      source: "/troubleshooting/:slug*",
+      destination: "/self-host-troubleshooting/:slug*",
+      permanent: true,
+    },
+    // redirect /upgrade to /self-host/upgrade
+    {
+      source: "/upgrade/:slug*",
+      destination: "/self-host-upgrade/:slug*",
+      permanent: true,
+    },
+  ]
+});
